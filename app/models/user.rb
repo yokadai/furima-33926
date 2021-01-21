71d888f4e9614a5686a79eee97af1c8dfae14532
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-21 15:36:59 +0900

    validation修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password,        format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6, }+\z/i}
+  validates :password,        format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
   validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name,       presence: true, format:{with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :first_name,      presence: true, format:{with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :last_name_kana,  presence: true, format:{with:/\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format:{with:/\A[ァ-ヶー－]+\z/}
   validates :birthday,        presence: true
 end
