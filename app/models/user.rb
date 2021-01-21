90cc566b2d0eb8a1487159d44f4f1a1be1dd4773
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-21 18:51:36 +0900

    with_optionsによりvalidationまとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-  validates :first_name,      presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday,        presence: true
+    with_options presence: true do
+      validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+      validates :nickname
+      validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+      validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+      validates :birthday
+     with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
+       validates :last_name
+       validates :first_name
+      end
+    end
+  end
 end
