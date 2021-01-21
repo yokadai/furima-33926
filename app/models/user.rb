eeedd7d255f3dcb0cf9305f3c981457132c51388
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-21 14:54:53 +0900

    passwordにvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password,        format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6, }+\z/i}
   validates :nickname,        presence: true
   validates :last_name,       presence: true
   validates :first_name,      presence: true
