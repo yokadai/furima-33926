5a1b94763ba258b3211596e8e73f9f376d1c15b2
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-22 18:17:38 +0900

    userモデルにバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :prefecture_id
+    validates :state_id
+    validates :price,          format: {with: /\A[0-9]+\z/}, :numericality => { :greater_than => 299 }, :numericality => { :less_than => 10000000} 
+    validates :shopping_fee_id
+    validates :shopping_day_id
+    validates :category_id
+    validates :user
+  end
 end
