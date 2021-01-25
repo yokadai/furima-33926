acc0199898974896c26a8b6d3e9b4d6411e88b60
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-25 18:06:17 +0900

    association追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
    
+    has_many :user_items
+    has_many :items dependent: :destroy
+
     with_options presence: true do
       validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
       validates :nickname
