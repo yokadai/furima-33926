70b99be6bd1ebc152368dacff973abb55adfbaf7
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-25 19:00:30 +0900

    validation再修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :prefecture_id
+    validates :state_id
+    validates :price,          format: {with: /\A[0-9]+\z/},:numericality => {
+      :greater_than_or_equal_to => 300,
+      :less_than_or_equal_to => 100000000
+   }
+    validates :shipping_fee_id
+    validates :shipping_day_id
+    validates :category_id
+    validates :user
+  end
+  
   has_one_attached       :image
   belongs_to             :user 
   has_one                :user_item
@@ -11,15 +26,5 @@ class Item < ApplicationRecord
   belongs_to_active_hash :Shipping_days
   
 
-  with_options presence: true do
-    validates :item_name
-    validates :description
-    validates :prefecture_id
-    validates :state_id
-    validates :price,          format: {with: /\A[0-9]+\z/}, :numericality => { :greater_than => 299 }, :numericality => { :less_than => 10000000} 
-    validates :shipping_fee_id
-    validates :shipping_day_id
-    validates :category_id
-    validates :user
-  end
+  
 end
