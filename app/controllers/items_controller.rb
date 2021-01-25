d622a6c955981887a53e03c54769aecf2b88ba11
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-25 15:12:06 +0900

    item_paramsメソッド、修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,17 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :image, :description, :shipping_fee_id, :shipping_day_id, :prefecture_id, :state_id, :price, :category_id).merge(user_id: current_user.id)
+  end
 end
