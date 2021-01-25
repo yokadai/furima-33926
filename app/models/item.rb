a7f44e8f2bae16a44399cb0738f865afe7da2be5
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-25 13:44:00 +0900

    active storage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :Shipping_fee
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :Shipping_days
-
+  has_one_attached       :image
 
   with_options presence: true do
     validates :item_name
