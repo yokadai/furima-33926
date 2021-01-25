d748fc412bb89f94514e33258a9eb81532847c34
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-25 13:31:59 +0900

    各選択項目にactive_hash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category_id
+  belongs_to_active_hash :state_id
+  belongs_to_active_hash :Shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :Shipping_days
+
+
   with_options presence: true do
     validates :item_name
     validates :description
     validates :prefecture_id
     validates :state_id
     validates :price,          format: {with: /\A[0-9]+\z/}, :numericality => { :greater_than => 299 }, :numericality => { :less_than => 10000000} 
-    validates :shopping_fee_id
-    validates :shopping_day_id
+    validates :shipping_fee_id
+    validates :shipping_day_id
     validates :category_id
     validates :user
   end
