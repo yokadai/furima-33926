01b277aab57cb05d24365687305247d0f924dfff
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-26 13:38:32 +0900

    itemモデル単体テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name       {"アイテム"}
+    description     {"説明"}
+    category_id     {1}
+    state_id        {1}
+    shipping_fee_id {1}
+    prefecture_id   {1}
+    shipping_day_id {1}
+    price           {300}
+    association     :user
+  
+      
+  
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do 
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    it "情報が正常であれば出品可能" do
+      expect(@item).to be_valid
+    end
+
+    it "画像が空だと保存できない" do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it "名前が空だと保存できない" do
+      @item.item_name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Item name can't be blank")
+    end
+
+    it "説明が空だと保存できない" do
+      @item.description = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+
+
+    it "カテゴリーが空だと保存できない" do
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+
+    it "状態が空だと保存できない" do
+      @item.state_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("State can't be blank")
+    end
+
+    it "配送手数料が空だと保存できない" do
+      @item.shipping_fee_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+    end
+
+    it "発送地域が空だと保存できない" do
+      @item.prefecture_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+    end
+
+    it "発送までの日数が空だと保存できない" do
+      @item.shipping_day_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+    end
+
+    it "値段が空だと保存できない" do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "値段が300未満だと保存できない" do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+
+    it "値段が10000000以上だと保存できない" do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+    end
+  end
 end
