7fe0a8c2f5328bfa0632e87031bdc84ebd7aaa93
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-27 14:57:43 +0900

    set_item に追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, except: [:index, :new, :create]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -35,8 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    if @item.destroy
       redirect_to root_path
     else
       render 'shared/error_messages', model: f.object 
