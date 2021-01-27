462afc9358e2124c91cb3ff795d459db197b6b0f
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-27 13:11:08 +0900

    destroyアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,11 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if item.destroy
+      redirect_to root_path
+    else
+      render 'shared/error_messages', model: f.object 
+    end
   end
 
   private
