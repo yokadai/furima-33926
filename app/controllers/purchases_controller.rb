a1c8e696c2cd395943baa0023c9f3bfb8645248d
yokadai <76767115+yokadai@users.noreply.github.com>
2021-02-09 09:55:13 +0900

    purchaseモデル/コントローラー作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,19 +1,24 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_purchase, only: [:index, :new ]
+  before_action :set_item, only: [:index, :create]
+  before_action :saler
+  
+  
   def index
-    @item = Item.find(params[:item_id])
   end
 
   def new
-    @purchase = Purchase.new
   end
 
   def create
+    user_id = current_user.id
     @purchase = Purchase.new(purchase_params)
     if @purchase.valid?
+      payJp
        @purchase.save
        redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render action: :index
     end
   end
@@ -21,8 +26,31 @@ class PurchasesController < ApplicationController
   private
   
   def purchase_params
-    params.permit(:postal_code, :prefecture_id, :city, :lot_number, :building, :phone_number, :user_id, :card_number, :card_exp_month, :card_exp_year, :card_cvc)
+    params.require(:purchase).permit(:postal_code, :prefecture_id, :city, :lot_number, :building, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
   end
+
+  def payJp
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price, 
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
+    end
+
+    def set_purchase
+      @purchase = Purchase.new
+    end
+
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
+
+    def saler
+      if current_user.id == @item.user_id
+        redirect_to root_path
+      end
+    end
 
 
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,22 +1,21 @@
 class Purchase
  
   include ActiveModel::Model
-  attr_accessor  :postal_code, :card_number, :card_exp_month, :card_exp_year, :card_cvc, :postal_code, :prefecture_id, :city, :lot_number, :building, :phone_number, :user_id
+  attr_accessor  :postal_code, :prefecture_id, :city, :lot_number, :building, :phone_number, :user_id, :item_id, :token
+
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ , message: "is invalid. Include hyphen(-)" }
     validates :city
-    validates :lot_number
+    validates :token
+    validates :prefecture_id
     with_options numericality: { only_integer: true } do
-      validates :card_number
-      validates :card_exp_month
-      validates :card_exp_year
-      validates :phone_number
+     validates :phone_number, format:{ with: /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/}
     end
   end
 
   def save
-    user = User.find(params[:id])
-    Donation.create(postal_code: postal_code, prefecture: prefecture_id, city: city, lot_number: lot_number, building: building, phone_number: phone_number, user_id: user.id)
+    Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, lot_number: lot_number, building: building, phone_number: phone_number ,item_id: item_id, user_id: user_id)
+    UserItem.create(item_id: item_id, user_id: user_id)
   end
 end
