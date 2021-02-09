37feb8055954f50519ac5e59d5b7e5d36a527679
yokadai <76767115+yokadai@users.noreply.github.com>
2021-02-09 12:32:06 +0900

    JS修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,9 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../new")
 require("../card")
+require("../new")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
